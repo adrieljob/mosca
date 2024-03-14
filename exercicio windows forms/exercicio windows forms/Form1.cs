743405e83bc7b76f0fc24ce93bea263cf680eeb2
adrieljob <thonyadriel@gmail.com>
2024-03-14 17:13:06 -0300

    exercicio forms1

---
+++ exercicio windows forms/exercicio windows forms/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exercicio_windows_forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+          {
+                string login = textBox1.Text;
+                string senha = textBox2.Text;
+
+                if (login == "admin" && senha == "ds@etefmc")
+
+                {
+                    MessageBox.Show("Login Completo!!\nParabens");
+                }
+
+                else
+
+                {
+                    MessageBox.Show("ERROR");
+                }
+            }
+
+        
+    }
+}
