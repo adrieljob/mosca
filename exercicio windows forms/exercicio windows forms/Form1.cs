b3f4ab2d3f2e13b4ae326d88b38661650328335e
adrieljob <thonyadriel@gmail.com>
2024-03-25 15:51:49 -0300

    mosca

--- exercicio windows forms/exercicio windows forms/Form1.cs
+++ exercicio windows forms/exercicio windows forms/Form1.cs
@@ -42,19 +42,45 @@ namespace exercicio_windows_forms
                 string login = textBox1.Text;
                 string senha = textBox2.Text;
 
-                if (login == "admin" && senha == "ds@etefmc")
+                if (login == "Login" && senha == "Senha")
 
                 {
-                    MessageBox.Show("Login Completo!!\nParabens");
-                }
+                Form2 j2 = new Form2();
+                j2.Show();
+            }
 
                 else
 
                 {
                     MessageBox.Show("ERROR");
                 }
-            }
+        }
 
-        
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+     
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Form6 j6 = new Form6();
+            j6.Show();
+
+        }
     }
 }
